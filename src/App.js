e198c3f318f0bc7efc90420233e831d010f7b99b
elderkampbell <elderkampbell@hotmail.com>
2022-09-08 17:55:05 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
         <a
           className="App-link"
