4779ba3d255dff29ff66354916c9809cb3628d35
RichardPMachado <richardmachado2211@gmail.com>
2022-09-12 18:41:44 -0300

    requisito 09 - completo

--- src/components/ProductsDetails.jsx
+++ src/components/ProductsDetails.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import { getProductDetailsById } from '../services/api';
 import addProduct from '../services/localStorage';
 import CartButton from './CartButton';
@@ -11,7 +11,7 @@ export default class ProductsDetails extends Component {
     thumbnail: '',
     price: '',
     id: '',
-    redirected: false,
+    // redirected: false,
     product: [],
   };
 
@@ -36,17 +36,14 @@ export default class ProductsDetails extends Component {
     const { product } = this.state;
     addProduct(product[0]);
     // console.log(product[0]);
-    this.setState({
-      redirected: true,
-    });
   };
 
   render() {
-    const { title, thumbnail, price, id, redirected } = this.state;
+    const { title, thumbnail, price, id } = this.state;
     return (
       <>
-        <CartButton />
         <div>
+          <CartButton />
           <p>{ id }</p>
           <h2 data-testid="product-detail-name">{ title }</h2>
           <h3 data-testid="product-detail-price">{ price }</h3>
@@ -63,7 +60,7 @@ export default class ProductsDetails extends Component {
             Add Carrinho
           </button>
         </div>
-        { redirected && <Redirect to="/shopping-cart/" /> }
+        {/* { redirected && <Redirect to="/shopping-cart/" /> } */}
       </>
     );
   }
